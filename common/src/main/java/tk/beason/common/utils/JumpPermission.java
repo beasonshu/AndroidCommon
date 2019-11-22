d176db1f481600f2ae1c9da98a35ad6488aedc48
shuxinghu <beasonshu@sina.com>
2019-11-22 17:32:04 +0800

    跳转权限管理界面

---
+++ common/src/main/java/tk/beason/common/utils/JumpPermission.java
@@ -0,0 +1,234 @@
+package tk.beason.common.utils;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
+
+
+import java.util.List;
+
+import tk.beason.common.BuildConfig;
+
+public class JumpPermission {
+    /**
+     * Build.MANUFACTURER
+     */
+    private static final String MANUFACTURER_HUAWEI = "Huawei";//华为
+    private static final String MANUFACTURER_MEIZU = "Meizu";//魅族
+    private static final String MANUFACTURER_XIAOMI = "Xiaomi";//小米
+    private static final String MANUFACTURER_SONY = "Sony";//索尼
+    private static final String MANUFACTURER_OPPO = "OPPO";
+    private static final String MANUFACTURER_LG = "LG";
+    private static final String MANUFACTURER_VIVO = "vivo";
+    private static final String MANUFACTURER_SAMSUNG = "samsung";//三星
+    private static final String MANUFACTURER_LETV = "Letv";//乐视
+    private static final String MANUFACTURER_ZTE = "ZTE";//中兴
+    private static final String MANUFACTURER_YULONG = "YuLong";//酷派
+    private static final String MANUFACTURER_LENOVO = "LENOVO";//联想
+
+    /**
+     * 此函数可以自己定义
+     * @param activity
+     */
+    public static void GoToSetting(Activity activity){
+        try {
+            switch (Build.MANUFACTURER){
+                case MANUFACTURER_HUAWEI:
+                    Huawei(activity);
+                    break;
+                case MANUFACTURER_MEIZU:
+                    Meizu(activity);
+                    break;
+                case MANUFACTURER_XIAOMI:
+                    Xiaomi(activity);
+                    break;
+                case MANUFACTURER_SONY:
+                    Sony(activity);
+                    break;
+                case MANUFACTURER_OPPO:
+                    OPPO(activity);
+                    break;
+                case MANUFACTURER_VIVO:
+                    goVivoMainager(activity);
+                    break;
+                case MANUFACTURER_LG:
+                    LG(activity);
+                    break;
+                case MANUFACTURER_LETV:
+                    Letv(activity);
+                    break;
+                default:
+                    ApplicationInfo(activity);
+                    Log.e("goToSetting", "目前暂不支持此系统");
+                    break;
+            }
+        }catch (Exception e){
+            ApplicationInfo(activity);
+        }
+
+    }
+
+    private static void goVivoMainager(Activity context) {
+        Intent localIntent;
+        if (((Build.MODEL.contains("Y85")) && (!Build.MODEL.contains("Y85A"))) || (Build.MODEL.contains("vivo Y53L"))) {
+            localIntent = new Intent();
+            localIntent.setClassName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.PurviewTabActivity");
+            localIntent.putExtra("packagename", context.getPackageName());
+            localIntent.putExtra("tabId", "1");
+            context.startActivity(localIntent);
+        } else {
+            localIntent = new Intent();
+            localIntent.setClassName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.SoftPermissionDetailActivity");
+            localIntent.setAction("secure.intent.action.softPermissionDetail");
+            localIntent.putExtra("packagename", context.getPackageName());
+            context.startActivity(localIntent);
+        }
+    }
+
+    public static void Huawei(Activity activity) {
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+        ComponentName comp = new ComponentName("com.huawei.systemmanager", "com.huawei.permissionmanager.ui.MainActivity");
+        intent.setComponent(comp);
+        activity.startActivity(intent);
+
+
+    }
+
+    public static void Meizu(Activity activity) {
+        Intent intent = new Intent("com.meizu.safe.security.SHOW_APPSEC");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+        activity.startActivity(intent);
+    }
+
+    public static void Xiaomi(Activity activity) {
+        Intent intent = new Intent("miui.intent.action.APP_PERM_EDITOR");
+        ComponentName componentName = new ComponentName("com.miui.securitycenter", "com.miui.permcenter.permissions.AppPermissionsEditorActivity");
+        intent.setComponent(componentName);
+        intent.putExtra("extra_pkgname", BuildConfig.APPLICATION_ID);
+        activity.startActivity(intent);
+    }
+
+    public static void Sony(Activity activity) {
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+        ComponentName comp = new ComponentName("com.sonymobile.cta", "com.sonymobile.cta.SomcCTAMainActivity");
+        intent.setComponent(comp);
+        activity.startActivity(intent);
+    }
+
+    public static void OPPO(Activity activity) {
+        doStartApplicationWithPackageName(activity,"com.coloros.safecenter");
+    }
+
+    public static void LG(Activity activity) {
+        Intent intent = new Intent("android.intent.action.MAIN");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+        ComponentName comp = new ComponentName("com.android.settings", "com.android.settings.Settings$AccessLockSummaryActivity");
+        intent.setComponent(comp);
+        activity.startActivity(intent);
+    }
+
+    public static void Letv(Activity activity) {
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+        ComponentName comp = new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.PermissionAndApps");
+        intent.setComponent(comp);
+        activity.startActivity(intent);
+    }
+
+    /**
+     * 只能打开到自带安全软件
+     * @param activity
+     */
+    public static void _360(Activity activity) {
+        Intent intent = new Intent("android.intent.action.MAIN");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+        ComponentName comp = new ComponentName("com.qihoo360.mobilesafe", "com.qihoo360.mobilesafe.ui.index.AppEnterActivity");
+        intent.setComponent(comp);
+        activity.startActivity(intent);
+    }
+
+    private static void doStartApplicationWithPackageName(Activity activity, String packagename) {
+        // 通过包名获取此APP详细信息，包括Activities、services、versioncode、name等等
+        PackageInfo packageinfo = null;
+        try {
+            packageinfo = activity.getPackageManager().getPackageInfo(packagename, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageinfo == null) {
+            return;
+        }
+        // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(packageinfo.packageName);
+        // 通过getPackageManager()的queryIntentActivities方法遍历
+        List<ResolveInfo> resolveinfoList = activity.getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+        Log.e("PermissionPageManager", "resolveinfoList" + resolveinfoList.size());
+        for (int i = 0; i < resolveinfoList.size(); i++) {
+            Log.e("PermissionPageManager", resolveinfoList.get(i).activityInfo.packageName + resolveinfoList.get(i).activityInfo.name);
+        }
+        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+        if (resolveinfo != null) {
+            // packageName参数2 = 参数 packname
+            String packageName = resolveinfo.activityInfo.packageName;
+            // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packageName参数2.mainActivityname]
+            String className = resolveinfo.activityInfo.name;
+            // LAUNCHER Intent
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            // 设置ComponentName参数1:packageName参数2:MainActivity路径
+            ComponentName cn = new ComponentName(packageName, className);
+            intent.setComponent(cn);
+            try {
+                activity.startActivity(intent);
+            } catch (Exception e) {
+                ApplicationInfo(activity);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 应用信息界面
+     * @param activity
+     */
+    public static void ApplicationInfo(Activity activity){
+        Intent localIntent = new Intent();
+        localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= 9) {
+            localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            localIntent.setData(Uri.fromParts("package", activity.getPackageName(), null));
+        } else if (Build.VERSION.SDK_INT <= 8) {
+            localIntent.setAction(Intent.ACTION_VIEW);
+            localIntent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+            localIntent.putExtra("com.android.settings.ApplicationPkgName", activity.getPackageName());
+        }
+        activity.startActivity(localIntent);
+    }
+
+    /**
+     * 系统设置界面
+     * @param activity
+     */
+    public static void SystemConfig(Activity activity) {
+        Intent intent = new Intent(Settings.ACTION_SETTINGS);
+        activity.startActivity(intent);
+    }
+}
