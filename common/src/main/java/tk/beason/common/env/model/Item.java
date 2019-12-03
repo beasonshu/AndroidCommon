5cad5fb0c1ff924a845388e6f0b788fe86591036
shuxinghu <beasonshu@sina.com>
2019-12-03 15:04:23 +0800

    解决fastjson无法解析的问题

---
+++ common/src/main/java/tk/beason/common/env/model/Item.java
@@ -0,0 +1,27 @@
+package tk.beason.common.env.model;
+
+public class Item {
+    public final String name;
+    public final boolean isEditable;
+    public String value;
+
+    public Item(){
+        this.name = "";
+        this.isEditable = false;
+    }
+    public Item(String name, String value, boolean isEditable) {
+        this.name = name;
+        this.value = value;
+        this.isEditable = isEditable;
+    }
+
+    public Item(String name, String value) {
+        this.name = name;
+        this.value = value;
+        isEditable = false;
+    }
+
+    public void updateValue(String value) {
+        this.value = value;
+    }
+}
--- common/src/main/java/tk/beason/common/env/model/Variable.java
+++ common/src/main/java/tk/beason/common/env/model/Variable.java
@@ -1,9 +1,8 @@
 package tk.beason.common.env.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Variable implements Serializable {
+public class Variable  {
 
     public String name;             // 名称
     public String desc;             // 说明
@@ -12,29 +11,6 @@ public class Variable implements Serializable {
 
     public String getValue() {
         return currentValue.value;
-    }
-
-    public static class Item implements Serializable {
-
-        public final String name;
-        public final boolean isEditable;
-        public String value;
-
-        public Item(String name, String value, boolean isEditable) {
-            this.name = name;
-            this.value = value;
-            this.isEditable = isEditable;
-        }
-
-        public Item(String name, String value) {
-            this.name = name;
-            this.value = value;
-            isEditable = false;
-        }
-
-        public void updateValue(String value) {
-            this.value = value;
-        }
     }
 
     public interface DefaultItemProvider {
